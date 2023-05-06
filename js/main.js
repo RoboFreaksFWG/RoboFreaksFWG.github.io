bdf2ff309101f787a9c8b3908f1a65f502befcdd
AthanasiusVI <jonathan.moses.tizian@gmail.com>
2023-05-06 14:43:24 +0200

    Statistics Zahlen Counter Fixes #7

---
+++ js/main.js
@@ -0,0 +1,49 @@
+/*-------------------------------------
+			Counting Numbers 
+-------------------------------------*/
+
+class CountUp {
+	constructor(triggerEl, counterEl, startOn) {
+	const counter = document.querySelector(counterEl)
+	const trigger = document.querySelector(triggerEl)
+	let num = startOn
+	const decimals = counter.dataset.decimals
+
+	const countUp = () => {
+	if (num < counter.dataset.stop)
+		
+		// Do we want decimals?
+		if (decimals) {
+		num += 0.01
+		counter.textContent = new Intl.NumberFormat('de', { 
+			minimumFractionDigits: 2,
+			maximumFractionDigits: 2 
+}).format(num)
+	}
+	else {
+		// No decimals
+		num++
+		counter.textContent = num
+	}
+	}
+	
+	const observer = new IntersectionObserver((el) => {
+	if (el[0].isIntersecting) {
+		const interval = setInterval(() => {
+		(num < counter.dataset.stop) ? countUp() : clearInterval(interval)
+		}, counter.dataset.speed)
+	}
+	}, { threshold: [0] })
+
+	observer.observe(trigger)
+	}
+}
+
+// Initialize any number of counters:
+new CountUp('#start1', '#count-fll', 0)
+new CountUp('#start1', '#count-lego', 14500)
+new CountUp('#start1', '#count-döner', 0)
+
+/*-------------------------------------
+			
+-------------------------------------*/
