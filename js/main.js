82d82e5c78160e726ac32c087c18cdbbff8960cd
Luca <lucadeckelmann@gmail.com>
2026-05-08 17:01:14 +0200

    Added Braunschweig Image Updated Counter

--- js/main.js
+++ js/main.js
@@ -127,7 +127,7 @@ class CountUp {
 
 // Initialize any number of counters:
 new CountUp('#start1', '#count-fll', 0)
-new CountUp('#start1', '#count-lego', 14500)
+new CountUp('#start1', '#count-lego', 25900)
 new CountUp('#start1', '#count-döner', 0)
 
 
